b2f557c18ac28f96c4c7b86cb2bdc29c5a7d7620
Jaime Bernal <jaimebernal@MacBook-Air-de-Jaime.local>
2024-04-10 11:04:09 -0600

    Logica de agregar personas

---
+++ src/components/ParticipantsList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>Participantes</h2>
+      <ul>
+        <li v-for="(participant, index) in participants" :key="index">{{ participant }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['participants']
+  };
+  </script>
+  
